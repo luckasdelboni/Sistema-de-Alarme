ce02e10418da4a52d8f66a3567d43f5f6c1d73f6
Luckas <luckas12@gmail.com>
2024-09-21 16:25:03 -0300

    Criada função para alternar os sistemas do alarme

--- alarmSystem.js
+++ alarmSystem.js
@@ -3,3 +3,9 @@ const readline = require('readline');
 
 let alarmeArmado = false // estado inicial do alarme
 let alarmeDisparado = false 
+
+// Função para ativar/desativar o alarme
+function alternarSistemaAlarme() {
+    alarmeAtivado = !alarmeAtivado;
+    console.log(`Sistema de alarme ${alarmeAtivado ? 'ATIVADO' : 'DESATIVADO'}`);
+}
